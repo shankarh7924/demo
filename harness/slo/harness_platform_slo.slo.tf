234531d54b9ef857b3a04c1542d032819b738ed6
shankarh7924 <124942580+shankarh7924@users.noreply.github.com>
2023-06-21 22:54:40 -0700

    Update harness_platform_slo.slo.tf

--- harness/slo/harness_platform_slo.slo.tf
+++ harness/slo/harness_platform_slo.slo.tf
@@ -12,7 +12,7 @@ resource "harness_platform_slo" "slo" {
     user_journey_refs = ["one", "two"]
     slo_target {
       type                  = "Calender"
-      slo_target_percentage = 99.5
+      slo_target_percentage = 99.9
       spec                  = jsonencode({
         type = "Monthly"
         spec = {
